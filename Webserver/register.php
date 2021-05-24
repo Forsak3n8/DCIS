30b05cc4433c29324a25ce68d63b983a64936241
Forsak3n8 <79763559+Forsak3n8@users.noreply.github.com>
2021-05-24 15:40:09 -0500

    Add files via upload

--- Webserver/register.php
+++ Webserver/register.php
@@ -20,7 +20,6 @@ if (empty($_POST['username']) || empty($_POST['password']) || empty($_POST['emai
 	// One or more values are empty.
 	exit('Please complete the registration form');
 }
-// We need to check if the account with that username exists.
 if (strlen($_POST['password']) > 20 || strlen($_POST['password']) < 5) {
 	exit('Password must be between 5 and 20 characters long!');
 }
@@ -30,6 +29,7 @@ if (preg_match('/^[a-zA-Z0-9]+$/', $_POST['username']) == 0) {
 if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
 	exit('Email is not valid!');
 }
+// We need to check if the account with that username exists.
 if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
 	// Bind parameters (s = string, i = int, b = blob, etc), hash the password using the PHP password_hash function.
 	$stmt->bind_param('s', $_POST['username']);
@@ -55,6 +55,10 @@ if ($stmt = $con->prepare('INSERT INTO accounts (username, password, email, acti
 	$message = '<p>Please click the following link to activate your account: <a href="' . $activate_link . '">' . $activate_link . '</a></p>';
 	mail($_POST['email'], $subject, $message, $headers);
 	echo 'Please check your email to activate your account!';
+} else {
+	// Something is wrong with the sql statement, check to make sure accounts table exists with all 3 fields.
+	echo 'Could not prepare statement!';
+}
 	}
 	$stmt->close();
 } else {
