50e57ef3e3739542931164eef9855fe7c87ae6e5
Forsak3n8 <79763559+Forsak3n8@users.noreply.github.com>
2021-05-25 16:56:43 -0500

    Add files via upload

--- Webserver/profile.php
+++ Webserver/profile.php
@@ -7,9 +7,9 @@ if (!isset($_SESSION['loggedin'])) {
 	exit;
 }
 $DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'root';
-$DATABASE_PASS = '';
-$DATABASE_NAME = 'phplogin';
+$DATABASE_USER = 'admin';
+$DATABASE_PASS = 'admin';
+$DATABASE_NAME = 'DICSIPS';
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if (mysqli_connect_errno()) {
 	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
@@ -29,7 +29,7 @@ $stmt->close();
 	<head>
 		<meta charset="utf-8">
 		<title>Profile Page</title>
-		<link href="style.css" rel="stylesheet" type="text/css">
+		<link href="stylesheet.css" rel="stylesheet" type="text/css">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
 	</head>
 	<body class="loggedin">
