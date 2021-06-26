c600e0bcbf58e7eb98dceb72c1161ca8d1df841b
Forsak3n8 <79763559+Forsak3n8@users.noreply.github.com>
2021-06-26 14:26:47 -0500

    Add files via upload

--- Webserver/changecans.php
+++ Webserver/changecans.php
@@ -14,6 +14,7 @@ if ( mysqli_connect_errno() ) {
 // Prepare our SQL, preparing the SQL statement will prevent SQL injection.
 $stmt = $con->prepare('SELECT title, qoh FROM cans');
 $stmt->execute();
+$stmt->store_result();
 $var = $stmt->fetchall();
 $var_dump($var);
 ?>
