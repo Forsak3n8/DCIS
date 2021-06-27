f19174cf3a38d0e8949dbc5c09b0fed205a7fa6d
Forsak3n8 <79763559+Forsak3n8@users.noreply.github.com>
2021-06-27 10:13:12 -0500

    Add files via upload

--- Webserver/changecans.php
+++ Webserver/changecans.php
@@ -15,6 +15,6 @@ if ( mysqli_connect_errno() ) {
 $stmt = $con->prepare('SELECT title, qoh FROM cans');
 $stmt->execute();
 $var = $stmt->get_result();
-$row = $var->fetch_array(MYSQLI_BOTH);
+$row = $var->fetch_array(MYSQLI_ASSOC);
 var_dump($row);
 ?>
