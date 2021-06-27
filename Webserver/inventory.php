5fc834f96773d853da3684a4629356fa1d2c11bf
Forsak3n8 <79763559+Forsak3n8@users.noreply.github.com>
2021-06-27 12:42:09 -0500

    Add files via upload

--- Webserver/inventory.php
+++ Webserver/inventory.php
@@ -5,6 +5,28 @@ session_start();
 if (!isset($_SESSION['loggedin'])) {
 	header('Location: index.html');
 	exit;
+
+// Database info
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'admin';
+$DATABASE_PASS = 'admin';
+$DATABASE_NAME = 'DCIS';
+// Database connection
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+if ( mysqli_connect_errno() ) {
+	// If there is an error with the connection, stop the script and display the error.
+	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+
+// Prepare our SQL, preparing the SQL statement will prevent SQL injection.
+$stmt = $con->prepare('SELECT title, qoh FROM cans');
+$stmt->execute();
+$var = $stmt->get_result();
+$rows = $var->fetch_all(MYSQLI_ASSOC);
+foreach ($rows as $row) {
+    printf("%s (%s)<br>", $row["title"], $row["qoh"]);
+
+$stmt->close();
 }
 ?>
 
