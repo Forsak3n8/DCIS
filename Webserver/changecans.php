9dafef542f5855f705d0aa928b4c9a80e4a69f05
Forsak3n8 <79763559+Forsak3n8@users.noreply.github.com>
2021-07-09 12:22:37 -0500

    Add files via upload

--- Webserver/changecans.php
+++ Webserver/changecans.php
@@ -1,4 +1,12 @@
 <?php
+// We need to use sessions, so you should always start sessions using the below code.
+session_start();
+// If the user is not logged in redirect to the login page...
+if (!isset($_SESSION['loggedin'])) {
+	header('Location: index.html');
+	exit;
+}
+
 // Database info
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'admin';
@@ -10,13 +18,19 @@ if ( mysqli_connect_errno() ) {
 	// If there is an error with the connection, stop the script and display the error.
 	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
+// Now we check if the data from the login form was submitted, isset() will check if the data exists.
+if ( !isset($_POST['countinput'], $_POST['title']) ) {
+	var_dump($_POST);
+}
 
 // Prepare our SQL, preparing the SQL statement will prevent SQL injection.
-$stmt = $con->prepare('SELECT title, qoh FROM cans');
-$stmt->execute();
-$var = $stmt->get_result();
-$rows = $var->fetch_all(MYSQLI_ASSOC);
-foreach ($rows as $row) {
-    printf("%s (%s)<br>", $row["title"], $row["qoh"]);
-}
-?>
+// $stmt = $con->prepare('SELECT title, qoh FROM cans');
+// $stmt->execute();
+// $var = $stmt->get_result();
+// $rows = $var->fetch_all(MYSQLI_ASSOC);
+// foreach ($rows as $row) {
+//     printf("%s (%s)<br>", $row["title"], $row["qoh"]);
+
+// $stmt->close();
+// }
+// ?>
