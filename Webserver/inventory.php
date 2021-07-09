8224a3769fab149a22a7623ca2f09d7156dee499
Forsak3n8 <79763559+Forsak3n8@users.noreply.github.com>
2021-07-09 12:39:11 -0500

    Add files via upload

--- Webserver/inventory.php
+++ Webserver/inventory.php
@@ -52,7 +52,7 @@ $stmt->close();
 				<?php
 				foreach ($rows as $row) {
 				echo "<fieldset class='inlineform'>";
-					echo "<input type='text' id='title' disabled placeholder='". $row['title'] ."'>";
+					echo "<input type='text' name='title' id='title' disabled placeholder='". $row['title'] ."'><br>";
 					echo "<label for='currentamount'>Current Amount:</label><br>";
 					echo "<input type='number' placeholder='". $row['qoh'] ."' id='countinput' name='countbox'><br>";
 					echo "<input type='submit' value='Submit'>";
